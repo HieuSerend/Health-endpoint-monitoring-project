953c22823e9ca18b3a8cbb308e323711b0800c4d
nchuy099 <nchihuy099@gmail.com>
2024-11-23 14:55:43 +0700

    commit

--- MonitoringWebsite/Backend/routes/goldApiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getGoldPrices } = require('../controllers/goldPriceApi');
-const router = express.Router();
-
-// Define the route for gold prices
-router.get('/gold-prices', getGoldPrices);
-
-module.exports = router;
